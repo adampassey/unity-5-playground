06137865b17623dcec4d585d11457b0bd6f5ff77
Adam Passey <adamjpassey@gmail.com>
2015-03-19 22:20:30 -0700

    Randomized wormholes

--- Assets/Scripts/Model/Wormhole.cs
+++ Assets/Scripts/Model/Wormhole.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class Wormhole : MonoBehaviour
 {
+	public enum Type
+	{
+		Home,
+		Wormhole 
+	}
+	;
+
+	public Type wormholeType = Type.Wormhole;
 
 	public void OnTriggerEnter2D (Collider2D collider)
 	{
 		Ship ship = collider.gameObject.GetComponent<Ship> ();
 		if (ship != null) {
-			Application.LoadLevel (Scene.MainScene);
+			if (wormholeType == Type.Home) {
+				Application.LoadLevel (Scene.MainScene);
+			} else {
+				Application.LoadLevel (Scene.SpaceScene);
+			}
 		}
 	}
 
