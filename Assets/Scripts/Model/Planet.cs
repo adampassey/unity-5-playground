85536a96a0aa53ae739dbc3098323c293ea035c5
Adam Passey <adamjpassey@gmail.com>
2015-03-19 22:01:13 -0700

    Adding Planet assets, scripts, randomization

---
+++ Assets/Scripts/Model/Planet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour
+{
+
+	public Vector3 spin = Vector3.right;
+	public int speed = 10;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Rotate (spin * Time.deltaTime * speed);
+	}
+}
--- Assets/Scripts/Scenes/SpaceScene.cs
+++ Assets/Scripts/Scenes/SpaceScene.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpaceScene : MonoBehaviour {
+public class SpaceScene : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
+	public int numberOfPlanets = 3;
+	public int minPlanetRadius = 500;
+	public int maxPlanetRadius = 1000;
+	public int maxPlanetSpin = 3;
+	public Vector2 bounds = new Vector2 (10000, 10000);
+
+	public GameObject planetPrefab;
+
+	public void Start ()
+	{
+		Randomize ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void Randomize ()
+	{
+		for (int i = 0; i < numberOfPlanets; i++) {
+
+			int scale = Random.Range (minPlanetRadius, maxPlanetRadius);
+			Vector3 planetPosition = new Vector3 (Random.Range (-bounds.x, bounds.x), Random.Range (-bounds.y, bounds.y), scale);
+			GameObject planetObject = GameObject.Instantiate (planetPrefab, planetPosition, Quaternion.identity) as GameObject;
+			planetObject.transform.localScale = new Vector3 (scale, scale, scale);
+
+			Planet planet = planetObject.GetComponent<Planet> () as Planet;
+			planet.spin.y = Random.Range (1, maxPlanetSpin);
+		}
 	}
 }
