30484f3ede8a1c983307b8432c7cdc9b900dc883
Adam Passey <adamjpassey@gmail.com>
2015-04-21 23:33:27 -0700

    Adding inventory and select ship button on main screen

---
+++ Assets/Scripts/UI/ModuleInventory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using AdamPassey.Inventory;
+
+public class ModuleInventory : MonoBehaviour {
+
+    private Inventory inventory;
+
+	// Use this for initialization
+	void Start () {
+        inventory = gameObject.GetComponent<Inventory>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!inventory.IsVisible()) {
+            inventory.Show();
+        }
+	}
+}
